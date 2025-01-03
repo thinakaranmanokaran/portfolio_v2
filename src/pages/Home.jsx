2389ae1ec1b3f2c44265a76dc358073db2a8fcff
Thinakaran <thinakaranmanohar@yahoo.com>
2025-01-03 18:52:58 +0530

    ENV ADDED

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ import { Helmet } from 'react-helmet-async';
 import { About, CommandPanel, Experience, LandingPage, Projects, Play, Footer } from "./../components/home/index";
 import StructuredData from '../components/seo/StructuredData';
 
+
 const Home = () => {
+    const siteLink = import.meta.env.VITE_SITE_LINK;
     return (
         <div className=' cursor-[url(./assets/images/cursor/lightcursor.png)] ' >
 <Helmet>
@@ -17,19 +19,19 @@ const Home = () => {
   
   <meta property="og:title" content="Thinakaran Manokaran | Developer & Designer" />
   <meta property="og:description" content="Explore the portfolio of Thinakaran Manokaran, a professional MERN stack developer based in Thanjavur." />
-  <meta property="og:image" content="https://dheena-portfolio-v2.vercel.app/preview.png" />
+  <meta property="og:image" content={`${siteLink}preview.png`} />
   <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
-  <meta property="og:url" content="https://dheena-portfolio-v2.vercel.app" />
+  <meta property="og:url" content={siteLink} />
   <meta property="og:type" content="website" />
   
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="Thinakaran Manokaran | Developer & Designer" />
-  <meta property="twitter:url" content="https://dheena-portfolio-v2.vercel.app" />
+  <meta property="twitter:url" content={siteLink} />
   <meta name="twitter:description" content="Explore the portfolio of Thinakaran Manokaran, a professional MERN stack developer based in Thanjavur." />
-  <meta name="twitter:image" content="https://dheena-portfolio-v2.vercel.app/preview.png" />
+  <meta name="twitter:image" content={`${siteLink}preview.png`} />
 
-  <link rel="canonical" href="https://dheena-portfolio-v2.vercel.app" />
+  <link rel="canonical" href={siteLink} />
 
 </Helmet>
 
